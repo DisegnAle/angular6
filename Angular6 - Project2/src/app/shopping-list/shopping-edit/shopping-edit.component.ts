73c2deeecf8f6af22f8c0ec40113816b2962d922
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2018-07-23 16:00:50 +0200

    dev project2

---
+++ Angular6 - Project2/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model'
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  //use @ViewChild component
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addIngredients(){
+    // IMPORTANT
+    const ingrName = this.nameInputRef.nativeElement.value;
+    const ingrAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingrName, ingrAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+}
